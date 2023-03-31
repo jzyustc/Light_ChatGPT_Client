fa722b74b3bdec23d8dc91f1e0700302e5ce1454
1730154258 <1730154258@qq.com>
2023-03-31 18:34:03 +0800

    change user window

--- GUI/window/user_window.py
+++ GUI/window/user_window.py
@@ -4,7 +4,7 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
 class UserWindow(QMainWindow):
-	user_window_close_signal = pyqtSignal(QMainWindow)	    # signal sent when the window closes
+	save_user_info_signal = pyqtSignal(QMainWindow)	    # signal to save the user information
 	
 	def __init__(self):
 		super().__init__()
@@ -71,7 +71,7 @@ class UserWindow(QMainWindow):
 
 		# button : close
 		self.button_switch_floating_window = QLabel()
-		self.button_switch_floating_window.mousePressEvent = self.on_window_close
+		self.button_switch_floating_window.mousePressEvent = lambda event : self.on_window_close()
 		self.button_switch_floating_window.setFixedSize(self.title_h - 10, self.title_h - 10)
 		self.button_switch_floating_window.setPixmap(QPixmap("data/images/close.png"))
 		self.button_switch_floating_window.setScaledContents(True)
@@ -93,20 +93,22 @@ class UserWindow(QMainWindow):
 		self.user_block.setFixedSize(self.w, self.h - self.title_h)
 		self.user_block_layout = QVBoxLayout()
 		self.user_block.setLayout(self.user_block_layout)
-		# self.user_block.setStyleSheet(f"border:1px solid #ccc;background-color:#888;")
 		self.user_block_layout.setContentsMargins(0, 0, 0, 0)
 		self.user_block_layout.setSpacing(0)
 
 		item_h = 50
 		self.uid_UI(item_h)
 		self.password_UI(item_h)
+		self.save_UI(item_h)
 		
         # add to user_block
 		self.user_block_layout.addStretch(2)
 		self.user_block_layout.addWidget(self.uid_block)
 		self.user_block_layout.addStretch(1)
 		self.user_block_layout.addWidget(self.password_block)
-		self.user_block_layout.addStretch(3)
+		self.user_block_layout.addStretch(2)
+		self.user_block_layout.addWidget(self.save_button_block)
+		self.user_block_layout.addStretch(2)
 
 		# add to main window
 		self.main_layout.addWidget(self.user_block)
@@ -123,9 +125,10 @@ class UserWindow(QMainWindow):
 
         ## uid title
 		self.uid_title = QLabel()
-		self.uid_title.setFixedSize(item_h, item_h)
-		self.uid_title.setText("uid : ")
+		self.uid_title.setFixedSize(int(item_h * 1.5), item_h)
+		self.uid_title.setText("uid")
 		self.uid_title.setFont(self.font)
+		self.uid_title.setAlignment(Qt.AlignCenter)
 
         ## uid text input
 		self.uid_text_input = QTextEdit()
@@ -154,9 +157,10 @@ class UserWindow(QMainWindow):
 
         ## password title
 		self.password_title = QLabel()
-		self.password_title.setFixedSize(item_h, item_h)
-		self.password_title.setText("password : ")
+		self.password_title.setFixedSize(int(item_h * 1.5), item_h)
+		self.password_title.setText("password")
 		self.password_title.setFont(self.font)
+		self.password_title.setAlignment(Qt.AlignCenter)
 
         ## password text input
 		self.password_text_input = QTextEdit()
@@ -172,6 +176,25 @@ class UserWindow(QMainWindow):
 		self.password_block_layout.addStretch(1)
 		self.password_block_layout.addWidget(self.password_text_input)
 		self.password_block_layout.addStretch(1)
+
+	def save_UI(self, item_h):
+		# button
+		self.save_button_block = QWidget()
+		self.save_button_block.setFixedSize(self.w, item_h)
+		self.save_button_block_layout = QHBoxLayout()
+		self.save_button_block.setLayout(self.save_button_block_layout)
+		self.save_button_block_layout.setContentsMargins(0, 0, 0, 0)
+		self.save_button_block_layout.setSpacing(0)
+
+		self.save_button = QPushButton("save")
+		self.save_button.setFont(self.font)
+		self.save_button.setFixedSize(self.w // 4, item_h)
+		self.save_button.setStyleSheet(f"border:1px solid #ccc;border-radius:5px;background-color:#f8f8f8;")
+		self.save_button.clicked.connect(self.on_save)
+		self.save_button_block_layout.addStretch(1)
+		self.save_button_block_layout.addWidget(self.save_button)
+		self.save_button_block_layout.addStretch(1)
+
 
 	'''
 	event
@@ -194,5 +217,15 @@ class UserWindow(QMainWindow):
 				# move event : canceling the moving
 				self.signal_move = False
 				
-	def on_window_close(self, event):
-		self.user_window_close_signal.emit(self)
+	def on_save(self, event):
+		self.save_user_info_signal.emit(self)
+
+	def on_window_close(self):
+		self.close()
+		self.deleteLater()
+
+	'''
+	control
+	'''
+	def set_pos(self, x, y):
+		self.move(x, y)
